55ed71dcb72d457062a6432281469b4a8e56efd0
Miki Grgić <miroslav.grgic@og-cs.com>
2025-10-13 13:10:30 +0200

    added new business use case

--- sb-hotel/src/main/java/hr/ogcs/eclipsestore/hotel/api/rpc/BookingRPCController.java
+++ sb-hotel/src/main/java/hr/ogcs/eclipsestore/hotel/api/rpc/BookingRPCController.java
@@ -9,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,11 +31,30 @@ public class BookingRPCController {
         return ResponseEntity.status(HttpStatus.OK).body(newBooking);
     }
 
+    @Tag(name = "Cancel and refund a Booking.",
+         description = "The returned Booking entity will contain a unique identifier for further processing.")
+    @PostMapping(value = "cancelAndRefund")
+    public ResponseEntity<Booking> cancelAndRefund(@RequestParam UUID id, @RequestParam RefundOption refundOption) {
+        // TODO not yet implemented
+        return ResponseEntity.accepted().build();
+    }
+
     @Tag(name = "Export Bookings by given date range.",
             description = "Date can be on daily, monthly or year base. The CSV will contain the fields:" +
                     "'id, roomNumber, guestName, from, to, price' separated by semicolons.")
-    @PostMapping(value = "/exportAllBookingsAsCSV", produces = "text/csv")
+    @PostMapping(value = "exportAllBookingsAsCSV", produces = "text/csv")
     public ResponseEntity<List<Object>> exportAllBookingsAsCSV(@RequestParam LocalDate from, @RequestParam LocalDate to) {
+        // TODO not yet implemented
+        return ResponseEntity.accepted().build();
+    }
+
+    @Tag(name = "Export Bookings by given date range.",
+            description = "Date can be on daily, monthly or year base. The file will contain the fields:" +
+                    "'id, roomNumber, guestName, from, to, price'.")
+    @PostMapping(value = "/exportAllBookingsByGivenFormat")
+    public ResponseEntity<List<Object>> exportAllBookingsByGivenFormat(
+            @RequestParam ExportFormat format,
+            @RequestParam LocalDate from, @RequestParam LocalDate to) {
         // TODO not yet implemented
         return ResponseEntity.accepted().build();
     }
@@ -45,7 +62,7 @@ public class BookingRPCController {
 
     @Tag(name = "Archive a Booking for long term persistence. ",
             description = "The archived Booking will not be available at API level any more.")
-    @PostMapping(value = "/archiveBooking")
+    @PostMapping(value = "archiveBooking")
     public ResponseEntity<Booking> archiveBooking(@RequestParam UUID id) {
         bookingPort.archiveBookingByID(id);
         return ResponseEntity.accepted().build();
@@ -53,10 +70,21 @@ public class BookingRPCController {
 
     @Tag(name = "Archive a Booking for long term persistence. ",
             description = "The archived Booking will not be available at API level any more.")
-    @GetMapping(value = "/archiveBooking/{id}")
+    @GetMapping(value = "archiveBooking/{id}")
     public ResponseEntity<Object> archiveBookingV2(@PathVariable(value = "id") UUID id) {
         bookingPort.archiveBookingByID(id);
         return ResponseEntity.accepted().build();
+    }
+
+    public enum ExportFormat {
+        CVS,
+        XLS,
+        JSON
+    }
+    public enum RefundOption {
+        PAYBACK,
+        VOUCHER,
+        NONE
     }
 
 }
